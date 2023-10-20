4544f20e3b19c00b69c8bb1f5dd118ff5b6ea097
Mahesh CHODA <choda.maheshbabu@gmail.com>
2023-10-20 19:20:08 +0530

    working responsive hero section

--- components/ui/Hero.tsx
+++ components/ui/Hero.tsx
@@ -32,42 +32,47 @@ const Hero = () => {
       </div>
 
       {/* Tablet and Laptop Layout */}
-      <div className="hidden md:block lg:flex justify-center items-start space-y-8 lg:space-y-0 lg:space-x-8">
-        {/* Left Part */}
-        <div className="w-full lg:w-1/2 p-4">
-          <div className="h-[50%] md:h-[70%] lg:h-[70%] w-full overflow-hidden mb-4">
-            <Image
-              src={leftImage}
-              alt="Left Image"
-              className="w-full h-full object-cover"
-              sizes="100vw"
-            />
-          </div>
-          <h2 className="text-2xl font-bold mb-2 text-left">
-            50 Shades of Self-Care: How to Give Yourself TLC for Every Occasion
-          </h2>
-          <p>Self-care isn't selfish.</p>
-        </div>
-        {/* Right Part */}
-        <div className="w-full lg:w-1/2 grid md:grid-rows-2 lg:grid-rows-3 gap-4">
-          {[1, 2, 3].map((item) => (
-            <div key={item} className="bg-white p-4 flex items-start">
-              <div className="w-2/5 h-full overflow-hidden mr-4">
-                <Image
-                  src={rightImage}
-                  alt={`Right Image ${item}`}
-                  className="w-full h-full object-cover"
-                  sizes="100vw"
-                />
-              </div>
-              <div className="w-3/5">
-                <h2 className="text-2xl text-left font-bold mb-2">
-                  Is Aluminum in Deodorant Something to Sweat About?
-                </h2>
-                <p>Is it *actually* bad for you?</p>
-              </div>
+      <div className="hidden md:block max-w-screen-lg mx-auto">
+        <div className="flex flex-col lg:flex-row justify-center items-start space-y-8 lg:space-y-0 lg:space-x-8">
+          {/* Left Part */}
+          <div className="w-full lg:w-1/2 p-4">
+            <div className="h-[70%] w-full overflow-hidden mb-4">
+              <Image
+                src={leftImage}
+                alt="Left Image"
+                className="w-full h-full object-cover"
+                sizes="100vw"
+              />
             </div>
-          ))}
+            <h2 className="text-2xl font-bold mb-2 text-left h-[25%]">
+              50 Shades of Self-Care: How to Give Yourself TLC for Every
+              Occasion
+            </h2>
+            <p className="text-left h-[5%]">Self-care isn't selfish.</p>
+          </div>
+          {/* Right Part */}
+          <div className="w-full lg:w-1/2 grid md:grid-rows-2 lg:grid-rows-3 gap-4">
+            {[1, 2, 3].map((item) => (
+              <div key={item} className="bg-white p-4 flex items-start">
+                <div className="w-2/5 h-full overflow-hidden mr-4">
+                  <Image
+                    src={rightImage}
+                    alt={`Right Image ${item}`}
+                    className="w-full h-full object-cover"
+                    sizes="100vw"
+                  />
+                </div>
+                <div className="w-3/5">
+                  <h2 className="text-2xl text-left font-bold mb-2 h-[80%]">
+                    Is Aluminum in Deodorant Something to Sweat About?
+                  </h2>
+                  <p className="h-[20%] text-left">
+                    Is it *actually* bad for you?
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
