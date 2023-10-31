29c13a60141f56bdd4523a7a14592d8645544273
Mahesh CHODA <choda.maheshbabu@gmail.com>
2023-10-31 14:15:19 +0530

    delete database from frontEnd

--- Database/actions/user.action.ts
+++
@@ -1,5 +0,0 @@
-"use server";
-
-import { connectToDB } from "./mongoose";
-
-connectToDB();
--- Database/models/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Article } from "./article.model";
-export { default as Author } from "./author.model";
-export { default as Category } from "./category.model";
