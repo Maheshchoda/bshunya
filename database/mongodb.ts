31a6cfaae1f98e07b80db126eb2af3bdc3643029
Mahesh CHODA <choda.maheshbabu@gmail.com>
2023-11-09 14:32:57 +0530

    avoiding making one more connection if already exist.

--- database/mongodb.ts
+++ database/mongodb.ts
@@ -4,11 +4,18 @@ const MONGODB_URI = process.env.MONGODB_URI;
 if (!MONGODB_URI) {
   throw new Error("MongoDB URL is not found.");
 }
+
 const client = new MongoClient(MONGODB_URI);
+let isConnected = false;
 
 async function connectToDatabase(): Promise<Db> {
+  if (isConnected) {
+    return client.db(); // Use the existing connection if it's already established
+  }
+
   try {
     await client.connect();
+    isConnected = true; // Set the flag to true once connected
     return client.db();
   } catch (error) {
     console.error("Connection to MongoDB failed:", error);
@@ -17,8 +24,13 @@ async function connectToDatabase(): Promise<Db> {
 }
 
 async function closeDatabaseConnection(): Promise<void> {
+  if (!isConnected) {
+    return; // No need to close if it's not connected
+  }
+
   try {
     await client.close();
+    isConnected = false; // Reset the flag once the connection is closed
   } catch (error) {
     console.error("Failed to close the database connection:", error);
     throw error;
